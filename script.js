7163071c9cee0005a592d1d0d048b4345ee9ccda
vijayapranesh <vijay14455@gmail.com>
2023-12-01 11:59:15 +0530

    task

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+MongoDB Day-2 Task
+
+Design database for Zen class programme
+users
+codekata
+attendance
+topics
+tasks
+company_drives
+mentors
+
+1 Find all the topics and tasks which are thought in the month of October
+2 Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+3 Find all the company drives and students who are appeared for the placement.
+4 Find the number of problems solved by the user in codekata
+5 Find all the mentors with who has the mentee's count more than 15
+6 Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+
